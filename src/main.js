ad88c4f769f93c95f5d6f323c078313ebc32e686
Aalok Shah <aalokshah1998@gmail.com>
2022-11-16 23:34:30 -0500

    updated manifest

--- src/main.js
+++ src/main.js
@@ -34,80 +34,45 @@ document.addEventListener("DOMContentLoaded", () => {
         const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
         // console.log(data);
 
+        const container = document.createElement("div");
+        container.setAttribute("class", "container");
+        document.body.appendChild(container);
+
         const weather = document.createElement("h2");
-        weather.setAttribute("id", "currentWeather");
+        weather.setAttribute("class", "currentWeather");
         weather.innerText = description;
-        document.querySelector("body").appendChild(weather);
+        document.querySelector(".container").appendChild(weather);
 
         const currentTemp = document.createElement("h1");
-        currentTemp.setAttribute("id", "temperature");
+        currentTemp.setAttribute("class", "temperature");
         currentTemp.innerText = `Current Temp: ${temp}\u00B0`;
-        document.querySelector("body").appendChild(currentTemp);
+        document.querySelector(".container").appendChild(currentTemp);
 
         const weatherIcon = document.createElement("img");
-        weatherIcon.setAttribute("id", "icon");
+        weatherIcon.setAttribute("class", "icon");
         weatherIcon.setAttribute("src", imageURL);
         weatherIcon.innerText = icon;
-        document.querySelector("body").appendChild(weatherIcon);
+        document.querySelector(".container").appendChild(weatherIcon);
 
         const feelsLikeTemp = document.createElement("div");
         feelsLikeTemp.setAttribute("class", "feelslike sm-text");
         feelsLikeTemp.innerText = `Feels Like: ${feelsLike}\u00B0`;
-        document.querySelector("body").appendChild(feelsLikeTemp);
+        document.querySelector(".container").appendChild(feelsLikeTemp);
 
         const tempMinDiv = document.createElement("div");
         tempMinDiv.setAttribute("class", "tempMin sm-text");
         tempMinDiv.innerText = `Low: ${tempMin}\u00B0`;
-        document.querySelector("body").appendChild(tempMinDiv);
+        document.querySelector(".container").appendChild(tempMinDiv);
 
         const tempMaxDiv = document.createElement("div");
         tempMaxDiv.setAttribute("class", "tempMax sm-text");
         tempMaxDiv.innerText = `High: ${tempMax}\u00B0`;
-        document.querySelector("body").appendChild(tempMaxDiv);
+        document.querySelector(".container").appendChild(tempMaxDiv);
 
         const humidityDiv = document.createElement("div");
         humidityDiv.setAttribute("class", "humidity sm-text");
         humidityDiv.innerText = `Humidity: ${humidity}`;
-        document.querySelector("body").appendChild(humidityDiv);
+        document.querySelector(".container").appendChild(humidityDiv);
       });
   });
 });
-
-// weather.innerHTML
-
-//  res.write("<p>The weather is currently " + description + "<p>");
-//  res.write(
-//    "<h1>The temperature in " + query + " is " + temp + " degrees.</h1>"
-//  );
-//  res.write("<img src=" + imageURL + ">");
-//  res.send();
-
-// app.post("/", function (req, res) {
-//   const query = req.body.cityName;
-//   const apiKey = "d4c55138bbca34363a7498b9f44c7c5d";
-//   const units = "imperial";
-//   const url =
-//     "https://api.openweathermap.org/data/2.5/weather?q=" +
-//     query +
-//     "&appid=" +
-//     apiKey +
-//     "&units=" +
-//     units;
-//   https.get(url, function (response) {
-//     console.log(response.statusCode);
-
-//     response.on("data", function (data) {
-//       const weatherData = JSON.parse(data);
-//       const temp = weatherData.main.temp;
-//       const description = weatherData.weather[0].description;
-//       const icon = weatherData.weather[0].icon;
-//       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-//       res.write("<p>The weather is currently " + description + "<p>");
-//       res.write(
-//         "<h1>The temperature in " + query + " is " + temp + " degrees.</h1>"
-//       );
-//       res.write("<img src=" + imageURL + ">");
-//       res.send();
-//     });
-//   });
-// });
